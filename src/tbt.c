3d7dc894fb4f6a629f33b254eb8bc020eae7cda0
CPietJa <tibane.galbrun@gmail.com>
2021-02-03 11:20:20 +0100

    Add Read File + Dir

--- src/tbt.c
+++ src/tbt.c
@@ -7,8 +7,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <dirent.h>
 #include <err.h>
+#include <errno.h>
 #include <getopt.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -46,6 +49,70 @@ static void version(void)
     printf("Triaging Binaries Toolkit %d.%d.%d\n", VERSION, SUBVERSION,
            REVISION);
     exit(EXIT_SUCCESS);
+}
+
+/**
+ * Treatment ELF File.
+ * Return false if problems, true otherwise.
+ */
+static bool treat_file(char *file_path)
+{
+    if (file_path == NULL)
+        return false;
+
+    /* Open ELF File */
+    FILE *f = fopen(file_path, "rb");
+    if (f == NULL)
+        return false;
+
+    /* Get Data */
+    elf_data data = elf_get_data(f);
+    fclose(f);
+    if (data == NULL)
+        return false;
+
+    /* Compute Fuzzy Hashing */
+    fprintf(stderr,
+            "[+] File '%s'\n"
+            "[+] Fuzzy Hashing\n",
+            file_path);
+    /* CTPH */
+    fprintf(stderr, "[+] \tCTPH ...\n");
+    /* LSH */
+    fprintf(stderr, "[+] \tLSH  ...\n");
+
+    /* Free Data */
+    elf_free(data);
+    return true;
+}
+
+/**
+ * Treatment Directory
+ */
+static bool treat_dir(char *dir_path)
+{
+    if (dir_path == NULL)
+        return false;
+
+    /* Open Dir */
+    DIR *dir = opendir(dir_path);
+    if (dir == NULL)
+        return false;
+
+    /* Get File(s) */
+    struct dirent *file;
+    char path[strlen(dir_path) + 1024];
+    while ((file = readdir(dir)) != NULL) {
+        sprintf(path, "%s%s", dir_path, file->d_name);
+        /* Treat File */
+        if (!treat_file(path))
+            if (verbose)
+                warnx("'%s' is an invalid file", file->d_name);
+    }
+
+    /* Close Dir */
+    closedir(dir);
+    return true;
 }
 
 /* MAIN */
@@ -105,19 +172,25 @@ int main(int argc, char *argv[])
     struct stat info;
 
     if (stat(argv[optind], &info) != 0)
-        errx(EXIT_FAILURE, "error: cannot access %s", argv[optind]);
-    else if (S_ISDIR(info.st_mode))
-        printf("%s is a directory\n", argv[optind]);
-    else if (S_ISREG(info.st_mode))
-        printf("%s is a regular file\n", argv[optind]);
-    else
-        errx(EXIT_FAILURE, "error: invalid file");
+        errx(EXIT_FAILURE, "error: cannot access '%s'", argv[optind]);
+    else if (S_ISDIR(info.st_mode)) {
+        fprintf(stderr, "'%s' is a directory\n", argv[optind]);
 
-    // FILE *f = fopen("/bin/wc", "rb");
-    // elf_data data = elf_get_data(f);
-    // fclose(f);
-    // elf_print_data(data);
-    // elf_free(data);
+        char dir_path[2048];
+        uint16_t i = strlen(argv[optind]);
+        if (argv[optind][i - 1] == '/')
+            sprintf(dir_path, "%s", argv[optind]);
+        else
+            sprintf(dir_path, "%s/", argv[optind]);
+
+        if (!treat_dir(dir_path))
+            error_exit = EXIT_FAILURE;
+    } else if (S_ISREG(info.st_mode)) {
+        fprintf(stderr, "'%s' is a regular file\n", argv[optind]);
+        if (!treat_file(argv[optind]))
+            errx(EXIT_FAILURE, "error: '%s' is an invalid file", argv[optind]);
+    } else
+        errx(EXIT_FAILURE, "error: invalid file");
 
     return error_exit;
 }
